262514da9fd4703d3d725b8cd1b9985ce9543975
Merom Green <narsimplyme@naver.com>
2017-12-10 17:17:44 +0900

    Scroll Lock

--- app/src/main/java/com/example/narsi/timespacemap/MapsActivity.java
+++ app/src/main/java/com/example/narsi/timespacemap/MapsActivity.java
@@ -166,7 +166,8 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapCli
         });
         mMap.setMinZoomPreference(14f);
         mMap.setMaxZoomPreference(14f);
-
+        mMap.getUiSettings().setScrollGesturesEnabled(false);
+        mMap.getUiSettings().setAllGesturesEnabled(false);
 
     }
 
