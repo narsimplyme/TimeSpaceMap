73e9cf90d85eecf8fbcacdd6a4560b8f8a23241e
woosung <sainteyes73@gmail.com>
2017-12-11 00:44:38 +0900

    인트로 영상 추가

---
+++ app/src/main/java/com/example/narsi/timespacemap/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.example.narsi.timespacemap;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class SplashActivity extends AppCompatActivity {
+    VideoView VideoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        VideoView=(VideoView)findViewById(R.id.videoView);
+
+        Uri video=Uri.parse("android.resource://" + getPackageName()+"/" +R.raw.intro);
+
+        VideoView.setVideoURI(video);
+
+        VideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                if(isFinishing())
+                    return;
+                startActivity(new Intent(SplashActivity.this, MapsActivity.class));
+                finish();
+            }
+        });
+        VideoView.start();
+    }
+}
